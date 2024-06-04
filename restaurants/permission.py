c4e229527ac09237710e671b832dbb4e840f00f0
abdulbosit19980204 <uzdev7@gmail.com>
2024-06-05 02:49:40 +0500

    Kerakli fayllar middleware.py, permission.py serializers.py simple codelar bilan qoshildi

---
+++ restaurants/permission.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class ExampleView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        content = {
+            'status': 'request was permitted'
+        }
+        return Response(content)
