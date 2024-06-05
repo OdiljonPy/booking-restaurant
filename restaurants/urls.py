ad71563e2fe04920503691b3f603007d9755c7e2
abdulbosit19980204 <uzdev7@gmail.com>
2024-06-06 02:09:36 +0500

    URL larni ozgartirdik.

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import RestaurantCategoryViewSet, RestaurantViewSet
+from .views import RestaurantCategoryViewSet, RestaurantViewSet, RoomTypeViewSet, RestaurantRoomViewSet
 
 urlpatterns = [
-    path('add/restaurant/', RestaurantViewSet.as_view({'post': 'add_restaurant', 'get': 'show_restaurant'})),
-    path('add/restaurant/category/', RestaurantCategoryViewSet.as_view({'get': 'restaurant_category'})),
+    # Abbos shu yerdagi restaurantlarni olib tashlash kerak:
+    # <sabab>bu yerga restaurant/ bilan keladi restaurant/add/restaurant/ xolatga kelib qoladi</sabab>
+    path('restaurant-category/', RestaurantCategoryViewSet.as_view({'get': 'restaurant_category'})),
+    path('/', RestaurantViewSet.as_view({'post': 'add_restaurant', 'get': 'show_restaurant'})),
+
+    # path('add/restaurant/', RestaurantViewSet.as_view({'post': 'add_restaurant', 'get': 'show_restaurant'})),
+    # path('add/restaurant/category/', RestaurantCategoryViewSet.as_view({'get': 'restaurant_category'})),
+
+    path('room-type/', RoomTypeViewSet.as_view({'get': 'restaurant_room'})),
+    path('rooms/', RestaurantRoomViewSet.as_view({'get': 'restaurant_room'})),
     # path('action/restaurant/<int:pk>', )
 ]
