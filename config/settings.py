628231c16253b5ca6bb43893de27f2c1e26676c3
abdulbosit19980204 <uzdev7@gmail.com>
2024-06-08 06:33:21 +0500

    Swagger qoshildi Settinglar nastroyka qilindi

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     # system packages
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_swagger',
     'drf_yasg',
 
     # local apps
@@ -64,7 +65,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
