cb07216c3c7eee1bd148102892874b05c6d65fe3
abdulbosit19980204 <uzdev7@gmail.com>
2024-06-08 17:09:36 +0500

    added booking url on config url.py

--- config/urls.py
+++ config/urls.py
@@ -35,6 +35,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/restaurant/', include("restaurants.urls")),
+    path('api/v1/booking/', include("booking.urls")),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
