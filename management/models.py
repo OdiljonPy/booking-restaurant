89ffcb9eb0c13b6682c08453928950d9202c3590
Farruh64 <rixsiboyevfaruuh@gmail.com>
2024-06-13 20:28:04 +0500

    model ozgardi

--- management/models.py
+++ management/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from restaurants.models import Restaurant
 
+
+class Manager(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    manager = models.ForeignKey(Manager, on_delete=models.SET_NULL, null=True, blank=True)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
 # Create your models here.
