42bebf8292b3da3d71e1badaba22c3698bbebbaa
MukhammadAli <rasulovadildora3@gmail.com>
2024-06-20 19:52:13 +0500

    changes

--- management/models.py
+++ management/models.py
@@ -1,74 +1,70 @@
-# from django.db import models
-# from django.utils import timezone
-#
-# class Department(models.Model):
-#     name = models.CharField(max_length=100)
-#     description = models.TextField(blank=True)
-#
-#     def str(self):
-#         return self.name
-#
-# class Role(models.Model):
-#     name = models.CharField(max_length=100)
-#     description = models.TextField(blank=True)
-#
-#
-#
-# class Manager(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True)
-#     title = models.CharField(max_length=100)
-#     phone_number = models.CharField(max_length=15, blank=True)
-#     date_of_birth = models.DateField(null=True, blank=True)
-#     hire_date = models.DateField(default=timezone.now)
-#
-#
-#
-# class Employee(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     manager = models.ForeignKey(Manager, on_delete=models.SET_NULL, null=True, blank=True, related_name='employees')
-#     department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True)
-#     role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True, blank=True)
-#     title = models.CharField(max_length=100)
-#     phone_number = models.CharField(max_length=15, blank=True)
-#     date_of_birth = models.DateField(null=True, blank=True)
-#     hire_date = models.DateField(default=timezone.now)
-#
-#
-#
-# class PerformanceReview(models.Model):
-#     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='reviews')
-#     reviewer = models.ForeignKey(Manager, on_delete=models.SET_NULL, null=True, blank=True)
-#     review_date = models.DateField(default=timezone.now)
-#     comments = models.TextField()
-#     rating = models.IntegerField()
-#
-#
-#
-# class Project(models.Model):
-#     name = models.CharField(max_length=100)
-#     description = models.TextField()
-#     department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True)
-#     start_date = models.DateField()
-#     end_date = models.DateField(null=True, blank=True)
-#     managers = models.ManyToManyField(Manager, related_name='projects')
-#
-#
-#
-# class Task(models.Model):
-#     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
-#     name = models.CharField(max_length=100)
-#     description = models.TextField()
-#     assigned_to = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True)
-#     due_date = models.DateField(null=True, blank=True)
-#     completed = models.BooleanField(default=False)
-#
-#
-#
-# class LeaveRequest(models.Model):
-#     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='leave_requests')
-#     start_date = models.DateField()
-#     end_date = models.DateField()
-#     reason = models.TextField()
-#     approved = models.BooleanField(default=False)
-#
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils import timezone
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def str(self):
+        return self.name
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+
+class Manager(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    hire_date = models.DateField(default=timezone.now)
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    manager = models.ForeignKey(Manager, on_delete=models.SET_NULL, null=True, blank=True, related_name='employees')
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    hire_date = models.DateField(default=timezone.now)
+
+
+class PerformanceReview(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='reviews')
+    reviewer = models.ForeignKey(Manager, on_delete=models.SET_NULL, null=True, blank=True)
+    review_date = models.DateField(default=timezone.now)
+    comments = models.TextField()
+    rating = models.IntegerField()
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    managers = models.ManyToManyField(Manager, related_name='projects')
+
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    assigned_to = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True)
+    due_date = models.DateField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+
+
+class LeaveRequest(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='leave_requests')
+    start_date = models.DateField()
+    end_date = models.DateField()
+    reason = models.TextField()
+    approved = models.BooleanField(default=False)
