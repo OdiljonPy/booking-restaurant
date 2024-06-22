79839fe7c7fbf286140bec98b4ebda9146589caf
abbos <hikmatullaevabbos24@gmail.com>
2024-06-22 15:40:10 +0500

    some models registered in admin panel

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import RestaurantCategory, Restaurant, RestaurantMenu, RestaurantMenuItem, RestaurantRoom, RoomType
+from .models import RestaurantCategory, Restaurant, RestaurantMenu, RestaurantMenuItem, RestaurantRoom, RoomType, \
+    MenuType, Comment
 
 admin.site.register(RestaurantCategory)
 admin.site.register(Restaurant)
@@ -7,4 +8,5 @@ admin.site.register(RestaurantMenu)
 admin.site.register(RestaurantMenuItem)
 admin.site.register(RestaurantRoom)
 admin.site.register(RoomType)
-
+admin.site.register(MenuType)
+admin.site.register(Comment)
