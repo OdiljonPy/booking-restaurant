523628656ea8046c3c8add4aa16ad0e90d4b62d0
Damir <nimadjonov.2404@gmail.com>
2024-06-22 18:23:09 +0500

    added reset password

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import UserViewSet, OtpViewSet, ChangePasswordViewSet
+from .views import UserViewSet, OtpViewSet, ChangePasswordViewSet, ResetPassword
 
 urlpatterns = [
     path('register/', UserViewSet.as_view({'post': 'create'}), name='register'),
     path('login/', UserViewSet.as_view({'post': 'login'}), name='login'),
     path('verify/', OtpViewSet.as_view({'post': 'verify'}), name='verify'),
     path('password/update/', ChangePasswordViewSet.as_view({'put': 'update'})),
+    path('password/send_token/', ResetPassword.as_view({'post': 'reset'}))
 
 ]
