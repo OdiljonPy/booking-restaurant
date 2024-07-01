ace2b9ede73e154c5f52d41f261c91c831f2d3d9
Farruh64 <rixsiboyevfaruuh@gmail.com>
2024-07-01 17:24:15 +0500

    some changes

--- management/models.py
+++ management/models.py
@@ -10,25 +10,24 @@ from booking.models import OrderItems, Occasion
 class Manager(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=15, validators=[validate_uz_number])
+    phone_number = models.CharField(max_length=14, validators=[validate_uz_number])
     date_of_birth = models.DateField(default=datetime.now)
-    hire_date = models.DateField(default=timezone.now)
+    hire_date = models.DateField(default=datetime.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user.username
+        return self.user.get_full_name()
 
 
 class Administrator(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    manager = models.ForeignKey(Manager, on_delete=models.SET_NULL)
-    title = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15, validators=[validate_uz_number])
-    date_of_birth = models.DateField(null=True, blank=True)
-    hire_date = models.DateField(default=timezone.now)
+    manager = models.ForeignKey(Manager, on_delete=models.SET_NULL, null=True)
+    phone_number = models.CharField(max_length=14, validators=[validate_uz_number])
+    date_of_birth = models.DateField(default=datetime.now)
+    hire_date = models.DateField(default=datetime.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def str(self):
-        return f'{self.user.get_full_name()} - {self.title}'
+    def __str__(self):
+        return self.user.get_full_name()
